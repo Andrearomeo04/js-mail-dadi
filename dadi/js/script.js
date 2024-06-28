b54d299df1f5fec1d9916e5998e46554187ba7d9
andrearomeo04 <pisco004@gmail.com>
2024-06-28 16:44:36 +0200

    [UPD] Created computer dice

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -3,5 +3,6 @@ const btn = document.getElementById('roll')
 btn.addEventListener('click', function () {
     console.log('dice rolled') 
     let human_dice = Math.floor(Math.random() * 6) + 1;
+    let computer_dice = Math.floor(Math.random() * 6) + 1;
 })
 
