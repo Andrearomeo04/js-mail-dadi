fd2950b760fa7dd274356cd6529fa428a5c9d4c4
andrearomeo04 <pisco004@gmail.com>
2024-07-01 04:20:05 +0200

    [UPD] Cicled mail_list and added condition

--- mail/js/script.js
+++ mail/js/script.js
@@ -3,7 +3,12 @@ const mail_list = ["andrea.romeo@gmail.com", "giancarlo.toscano@gmail.com", "ser
 let btn = document.getElementById('check')
 
 btn.addEventListener('click', function () {
-    document.getElementById('list')
+    let mail = document.getElementById('list').ariaValueMax.toLowerCase();
 
     let list_check = false
+
+    for(i = 0; i < mail_list.length; i++)
+        if (mail_list[i].toLowerCase() === mail) {
+            list_check = true;
+        }
 })
