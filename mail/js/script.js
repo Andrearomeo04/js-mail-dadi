128359d7bea5d39d189e10bf7a2bad382fa331ac
andrearomeo04 <pisco004@gmail.com>
2024-07-01 04:13:49 +0200

    [UPD] added event listener to button and created a control variable

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,9 @@
 const mail_list = ["andrea.romeo@gmail.com", "giancarlo.toscano@gmail.com", "sergio.trentini@gmail.com","vincenzo.baresi@gmail.com", "patrizio.cattaneo@gmail.com"];
+
+let btn = document.getElementById('check')
+
+btn.addEventListener('click', function () {
+    document.getElementById('list')
+
+    let list_check = false
+})
