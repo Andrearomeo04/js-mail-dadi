3f433625697f00cc375f288390411ff84500af82
andrearomeo04 <pisco004@gmail.com>
2024-07-01 04:27:17 +0200

    [UPD] Added final message

--- mail/js/script.js
+++ mail/js/script.js
@@ -3,12 +3,21 @@ const mail_list = ["andrea.romeo@gmail.com", "giancarlo.toscano@gmail.com", "ser
 let btn = document.getElementById('check')
 
 btn.addEventListener('click', function () {
-    let mail = document.getElementById('list').ariaValueMax.toLowerCase();
+    let mail = document.getElementById('list').value.toLowerCase();
 
     let list_check = false
 
-    for(i = 0; i < mail_list.length; i++)
+    for(i = 0; i < mail_list.length; i++){
         if (mail_list[i].toLowerCase() === mail) {
             list_check = true;
         }
+    }
+
+    if (list_check === true) {
+        console.log('access granted. Associated mail found')
+    }
+    else {
+        console.log('Access denied. Mail not found')
+    }
+        
 })
