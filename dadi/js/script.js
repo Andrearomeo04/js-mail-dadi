fb81281729bd592a7796f5a7b835373027df49b9
andrearomeo04 <pisco004@gmail.com>
2024-07-01 03:33:29 +0200

    [UPD] added if condition

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -3,6 +3,18 @@ const btn = document.getElementById('roll')
 btn.addEventListener('click', function () {
     console.log('dice rolled') 
     let human_dice = Math.floor(Math.random() * 6) + 1;
+    console.log(`Human ha ottenuto il numero ${human_dice}`)
     let computer_dice = Math.floor(Math.random() * 6) + 1;
+    console.log(`Computer ha ottenuto il numero ${computer_dice}`)
+
+    if (human_dice > computer_dice) {
+        console.log('Human has Won!')
+    }
+    else if (human_dice < computer_dice) {
+        console.log('Computer has Won!')
+    }
+    else {
+        console.log('è un pareggio!')
+    }
 })
 
